fc5f9368f5f4260e414b95758ca4be40719af5be
Jonghyeok <jhpmuscle@gmail.com>
2023-11-20 18:05:35 +0900

    feat: chat, login

--- Frontend/public/js/chat.js
+++ Frontend/public/js/chat.js
@@ -1,9 +1,77 @@
 const messageContainer = $(".message-container");
 const messageSend = $("#content");
 const sendButton = $("#send");
+const toggleBtn = $("#toggle-btn");
+const sideMenu = $(".side-menu");
+const closeBtn = $("#close-btn");
+const login = $("#login");
+const loginModal = $(".login-modal");
+const modalBack = $(".login-modal-background");
+const submit = $('#submit');
+const id = $('#id');
+const pw = $('#pw');
+
+login.click(function (e) {
+    loginModal.css('opacity', '1');
+    modalBack.css('opacity', '1');
+    loginModal.css('visibility', 'visible');
+    modalBack.css('visibility', 'visible');
+})
+
+modalBack.click(function (e) {
+    loginModal.css('opacity', '0');
+    modalBack.css('opacity', '0');
+    setTimeout(function () {
+        loginModal.css('visibility', 'hidden');
+        modalBack.css('visibility', 'hidden');
+    }, 1000);
+})
+toggleBtn.on('click', function (e) {
+    sideMenu.css('transform', 'translateX(0)')
+});
+
+closeBtn.on('click', function (e) {
+    sideMenu.css('transform', 'translateX(300px)')
+})
+
+submit.on('click', function (e) {
+    if (id.val().length > 0 && pw.val().length > 0) {
+        const name = id.val().toString();
+        const pass = pw.val().toString();
+        const data = {
+            username: name,
+            password: pass
+        };
+        console.log(name);
+        console.log(pass);
+
+        fetch(config.serverUrl + "/login", {
+            method: "POST",
+            body: JSON.stringify(data),
+        }).then(res => {
+            const token = res.headers.get("Authorization").substring(6)
+            setCookie("AccessToken", token, 60);
+        }).catch(err => {
+            console.log(err);
+        })
+        // $.ajax({
+        //     type: 'POST',
+        //     url: 'http://43.202.9.254:8080/login',
+        //     // dataType: 'json',
+        //     data: JSON.stringify(data),
+        //     success: function (res, status, xhr) {
+        //         console.log("success");
+        //         console.log(xhr.getAllResponseHeaders());
+        //     },
+        //     error: function (err) {
+        //         console.log(err);
+        //     }
+        // })
+    }
+})
 
 messageSend.on('keydown', (e) => {
-    if (e.keyCode == 13 && !(sendButton.prop("disabled"))) {
+    if (e.keyCode === 13 && !(sendButton.prop("disabled"))) {
         e.preventDefault();
         sendButton.click();
     }
@@ -53,10 +121,9 @@ sendButton.click(() => {
 
         let data = { question: content };
         $.ajax({
-            url: "http://43.202.9.254:8000/chat",
+            url: config.chatUrl + "/chat",
             type: "GET",
             data: data,
-            dataType: 'json',
             success: function (res) {
                 let response = res.answer;
                 console.log(response);
